d4c61dbd19c23587651035512477c1c4e69748db
MidouMir <MirMohamedLamine@gmail.com>
2017-03-06 01:19:27 +0100

    Ajout ModifCompte

--- app/src/main/java/comic/systems/ppefood/Compte.java
+++ app/src/main/java/comic/systems/ppefood/Compte.java
@@ -20,6 +20,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,6 +29,7 @@ import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -46,7 +49,15 @@ public class Compte extends AppCompatActivity {
     public static final int CONNECTION_TIMEOUT=10000;
     public static final int READ_TIMEOUT=15000;
     public ImageView menuOverflow;
-    private FloatingActionButton logout;
+    private Button saveCmpt;
+    private Button logout;
+    private EditText cptNom;
+    private EditText cptPrenom;
+    private EditText cptAdresse;
+    private EditText cptComm;
+    private EditText cptSiret;
+    private EditText cptMail;
+    private TextView cptType;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,7 +78,8 @@ public class Compte extends AppCompatActivity {
         upArrow.setColorFilter(getResources().getColor(R.color.colorAccent), PorterDuff.Mode.SRC_ATOP);
         getSupportActionBar().setHomeAsUpIndicator(upArrow);
 
-        logout = (FloatingActionButton) findViewById(R.id.fabLogout);
+        /*logout = (FloatingActionButton) findViewById(R.id.fabLogout);*/
+        logout = (Button) findViewById(R.id.btnLogout);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)  {
@@ -79,6 +91,33 @@ public class Compte extends AppCompatActivity {
         new AsyncFetch(user).execute();
         TextView titre = (TextView)findViewById(R.id.titreTitre);
         titre.setText(user);
+
+        // enregistrer les informations
+        saveCmpt = (Button) findViewById(R.id.btnSave);
+        // trouver les champs
+        cptNom          = (EditText) findViewById(R.id.compteNom);
+        cptPrenom       = (EditText) findViewById(R.id.comptePrenom);
+        cptAdresse      = (EditText) findViewById(R.id.compteAdresse);
+        cptComm         = (EditText) findViewById(R.id.compteDetails);
+        cptSiret        = (EditText) findViewById(R.id.compteSiret);
+        cptMail         = (EditText) findViewById(R.id.titreTitre);
+        cptType         = (TextView) findViewById(R.id.compteType);
+        // envoyer les donnéees du formulaire
+        saveCmpt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)  {
+                new AsyncUpdate().execute(
+                        "id-"+saveCmpt.getId(),
+                        cptNom.getText().toString(),
+                        cptPrenom.getText().toString(),
+                        cptAdresse.getText().toString(),
+                        cptComm.getText().toString(),
+                        cptSiret.getText().toString(),
+                        cptMail.getText().toString(),
+                        cptType.getText().toString()
+                );
+            }
+        });
     }
 
     private void confirmDialog() {
@@ -213,18 +252,28 @@ public class Compte extends AppCompatActivity {
                     String siret = json_data.getString("siret");
                     String commentaires = json_data.getString("commentaires");
                     String typeCompte = json_data.getString("typeCompte");
-                    String adresseFacturation = json_data.getString("adresseFacturation");
+                    String adresse = json_data.getString("adresse");
                     String cpU = json_data.getString("cpU");
                     String nbCommande = json_data.getString("nbCommande");
 
+                    Button compteValid      = (Button)findViewById(R.id.btnSave);
+                    compteValid.setId(idU);
+
                     // Nom
-                    TextView compteNom = (TextView)findViewById(R.id.produitTitre);
+                    TextView compteNom      = (TextView)findViewById(R.id.compteNom);
+                    TextView comptePrenom   = (TextView)findViewById(R.id.comptePrenom);
 
                     // Adresse
-                    TextView compteAdresse = (TextView)findViewById(R.id.produitDesc);
+                    TextView compteAdresse  = (TextView)findViewById(R.id.compteAdresse);
+                    TextView compteDetailsT = (TextView)findViewById(R.id.titreDetails);
+                    TextView compteDetails  = (TextView)findViewById(R.id.compteDetails);
+                    TextView compteSiretT   = (TextView)findViewById(R.id.titreSiret);
+                    TextView compteSiret    = (TextView)findViewById(R.id.compteSiret);
+
+                    compteAdresse.setText(adresse);
 
                     // Type de compte
-                    TextView compteType = (TextView)findViewById(R.id.produitCat);
+                    TextView compteType = (TextView)findViewById(R.id.compteType);
                     compteType.setText("Compte " + typeCompte);
 
                     // Type de compte
@@ -233,10 +282,20 @@ public class Compte extends AppCompatActivity {
 
                     if(typeCompte.equals("Entreprise")){
                         compteNom.setText(nom);
-                        compteAdresse.setText("Adresse de facturation:\n"+adresseFacturation+"\n\n"+commentaires+"\n\nNuméro de SIRET: "+siret);
+                        comptePrenom.setVisibility(View.GONE);
+                        compteDetailsT.setText("Complément");
+                        compteDetails.setText(commentaires);
+                        compteSiretT.setText("Numéro de SIRET");
+                        compteSiret.setText(siret);
                     }else {
-                        compteNom.setText(prenom + " " + nom);
-                        compteAdresse.setText("Code postal défini: " + cpU);
+                        comptePrenom.setText(prenom);
+                        comptePrenom.setWidth(50);
+                        compteNom.setText(nom);
+                        compteNom.setWidth(50);
+                        compteDetailsT.setVisibility(View.GONE);
+                        compteDetails.setVisibility(View.GONE);
+                        compteSiretT.setVisibility(View.GONE);
+                        compteSiret.setVisibility(View.GONE);
                     }
 
                 } catch (JSONException e) {
@@ -277,5 +336,117 @@ public class Compte extends AppCompatActivity {
             }
         }
 
+    }
+
+    // Create class AsyncUpdate
+    private class AsyncUpdate extends AsyncTask<String, String, String> {
+
+        ProgressDialog pdLoading = new ProgressDialog(Compte.this);
+        HttpURLConnection conn;
+        URL url = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //this method will be running on UI thread
+            pdLoading.setMessage("\tChargement...");
+            pdLoading.setCancelable(false);
+            pdLoading.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                // Enter URL address where your php file resides
+                url = new URL("https://demo.comic.systems/android/majCompte");
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return e.toString();
+            }
+            try {
+                // Setup HttpURLConnection class to send and receive data from php and mysql
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(READ_TIMEOUT);
+                conn.setConnectTimeout(CONNECTION_TIMEOUT);
+                conn.setRequestMethod("POST");
+
+                // setDoInput and setDoOutput to true as we send and recieve data
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                // add parameter to our above url
+                Uri.Builder builder = new Uri.Builder()
+                        .appendQueryParameter("id", params[0])
+                        .appendQueryParameter("nom", params[1])
+                        .appendQueryParameter("prenom", params[2])
+                        .appendQueryParameter("adresse", params[3])
+                        .appendQueryParameter("commentaires", params[4])
+                        .appendQueryParameter("siret", params[5])
+                        .appendQueryParameter("mail", params[6])
+                        .appendQueryParameter("typeCompte", params[7]);
+                /*
+                        .appendQueryParameter("id", id)
+                        .appendQueryParameter("nom", nom)
+                        .appendQueryParameter("prenom", prenom)
+                        .appendQueryParameter("mail", mail)
+                        .appendQueryParameter("adresse", adresse)
+                        .appendQueryParameter("commentaires", commentaires)
+                        .appendQueryParameter("siret", siret)
+                        .appendQueryParameter("typeCompte", typeCompte);*/
+                String query = builder.build().getEncodedQuery();
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+                conn.connect();
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+                return e1.toString();
+            }
+
+            try {
+                int response_code = conn.getResponseCode();
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+                    // Read data sent from server
+                    InputStream input = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+                    // Pass data to onPostExecute method
+                    return (result.toString());
+                } else {
+                    return("Connection error");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return e.toString();
+            } finally {
+                conn.disconnect();
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String maj) {
+            //this method will be running on UI thread
+            pdLoading.dismiss();
+            if(maj.contains("erreur")) {
+                Toast.makeText(Compte.this, "Impossible de mettre à jour votre compte ("+maj+")", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(Compte.this, "Votre compte a bien été mis à jour !", Toast.LENGTH_LONG).show();
+            }
+
+        }
     }
 }
